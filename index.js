4e9f8bdda22e3b7a92e2d2c9245d281e36cac624
Jenny Gebske <jennifer@gebske.de>
2019-05-08 20:00:21 +0200

    indent properly

--- index.js
+++ index.js
@@ -5,24 +5,22 @@ const mysql = require('mysql')
 const BodyParser = require('koa-bodyparser')
 const mount = require('koa-mount')
 
-
 const {
 	PORT = 9000,
-  DATABASE_USER,
-  DATABASE_PASSWORD,
-  DATABASE_HOST,
-  DATABASE_PORT,
-  DATABASE_NAME
+		DATABASE_USER,
+		DATABASE_PASSWORD,
+		DATABASE_HOST,
+		DATABASE_PORT,
+		DATABASE_NAME
 } = process.env
 
-
 function createRouter() {
-  const router = new Router()
+	const router = new Router()
 	// define endpoints
 	router.get('/', (ctx, next) => {
-    ctx.body = 'Hello World!'
+		ctx.body = 'Hello World!'
 	})
-  return router
+	return router
 }
 
 async function main() {
@@ -36,35 +34,35 @@ async function main() {
 		timezone: 'UTC'
 	})
 
-	client.connect(function(err) {
+	client.connect(function (err) {
 		if (err) {
 			console.error('error connecting: ' + err.stack)
 			return
 		}
-	 
+
 		console.log('connected as id ' + client.threadId)
 	})
 
 	// use client to create db tables
 
-  const router = createRouter()
-  const app = new Koa()
+	const router = createRouter()
+	const app = new Koa()
 
-  app.use(cors())
-    .use(BodyParser())
-    .use(mount(router.routes()))
-    .use(router.allowedMethods())
+	app.use(cors())
+		.use(BodyParser())
+		.use(mount(router.routes()))
+		.use(router.allowedMethods())
 
-  // start server
-  app.listen(PORT, function() {
-    process.stdout.write(`Server now listening on port ${PORT}\n`)
+	// start server
+	app.listen(PORT, function () {
+		process.stdout.write(`Server now listening on port ${PORT}\n`)
 	})
 }
 
 // call main() and catch errors
 main().catch(err => {
-  process.stderr.write(`${err.message}\n`);
-  process.exit(1);
+	process.stderr.write(`${err.message}\n`);
+	process.exit(1);
 });
 
 module.exports = main
